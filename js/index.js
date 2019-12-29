00e9292766c21f4d097b7a1b520cbf3fe9efb68d
Aleksey <ekbdizzy@yandex.ru>
2019-12-29 17:40:49 +0300

    added timer

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', function () {
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
+    // tabs
     function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
@@ -32,7 +33,53 @@ window.addEventListener('DOMContentLoaded', function () {
                 }
             }
         }
-    })
+    });
+
+    // timer
+
+    let deadline = '2019-12-30',
+        id = 'timer';
+
+    function getTimeRemaining(endtime) {
+        let delta = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((delta / 1000) % 60),
+            minutes = Math.floor((delta / 1000 / 60) % 60),
+            hours = Math.floor(delta / (1000 * 60 * 60));
+        return {
+            'total': delta,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setTime(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateTime, 1000);
+
+        function checkTwoDigits(value) {
+            return (value < 10) ? ('0' + value) : value;
+        }
+
+        function updateTime() {
+            let currentTime = getTimeRemaining(endtime);
+            hours.textContent = currentTime.hours;
+            minutes.textContent = checkTwoDigits(currentTime.minutes);
+            seconds.textContent = checkTwoDigits(currentTime.seconds);
+
+            if (currentTime.total <= 0) {
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setTime(id, deadline);
 });
 
 
